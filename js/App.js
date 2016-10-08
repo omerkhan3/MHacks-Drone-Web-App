33d4f87b0b92dcbe089f51dce6cb5d62a1491aaf
Loren Heubert-Aubry <lorenha@umich.edu>
2016-10-08 02:05:32 -0400

    added initial firebase push

--- js/App.js
+++ js/App.js
@@ -1,1 +1,13 @@
 // App Javascript File
+
+$(document).ready(){
+
+  var geodata = "here";
+  var alertsRef = firebase.database().child("alerts");
+  $("emergency").on('click', function(){
+    alertsRef.child().set({
+      location: geodata;
+    })
+  });
+
+});
