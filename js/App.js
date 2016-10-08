814d5a43cd225e12976d787f783abb5a92255608
Loren Heubert-Aubry <lorenha@umich.edu>
2016-10-08 02:09:18 -0400

    fixed bug

--- js/App.js
+++ js/App.js
@@ -4,10 +4,11 @@ $(document).ready(){
 
   var geodata = "here";
   var alertsRef = firebase.database().child("alerts");
-  $("emergency").on('click', function(){
+  $(".emergency").on('click', function(){
     alertsRef.child().set({
       location: geodata;
     })
   });
 
 });
+-
