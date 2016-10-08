0eea585505975e1070ef8d2a58189e2e0b09a39c
Loren Heubert-Aubry <lorenha@umich.edu>
2016-10-08 02:12:49 -0400

    properly called App.js

--- js/App.js
+++ js/App.js
@@ -4,6 +4,7 @@ $(document).ready(){
 
   var geodata = "here";
   var alertsRef = firebase.database().child("alerts");
+  console.log("Hello!");
   $(".emergency").on('click', function(){
     alertsRef.child().set({
       location: geodata;
